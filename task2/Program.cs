03c85fc8e39e7fb6b4fbaf39d94f58d3b10544e4
Dmitrii_Serebriakov <serebryakovdk@gmail.com>
2024-03-06 00:55:26 +0300

    2 program is written

---
+++ task2/Program.cs
@@ -0,0 +1,36 @@
+﻿
+string[] str = { "Volvo", "BMW", "Ford", "Mazda", "Uaz", "Kia" };
+
+string[] Pick3symbols(string[] str)
+{
+    int CountSize = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (str[i].Length <= 3)
+            CountSize++;
+    }
+
+    string[] StringArrayof3Symbols = new string[CountSize];
+    int j = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (str[i].Length <= 3)
+        {
+            StringArrayof3Symbols[j] = str[i];
+            j++;
+        }
+    }
+    return StringArrayof3Symbols;
+}
+
+void printArray(string[] str)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < str.Length; i++)
+    {
+        Console.Write($"{str[i]}; ");
+    }
+    Console.Write("]");
+    }
+    
+printArray(Pick3symbols(str));
